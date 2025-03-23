eae1aaee29eae47c73dcc5901d4877b0f923f4d7
daniel-gomes-silva <up201909935@fe.up.pt>
2025-03-23 16:25:23 +0000

    Add checks for duplicate fields, methods, parameters and imports

--- src/main/pt/up/fe/comp2025/symboltable/JmmSymbolTableBuilder.java
+++ src/main/pt/up/fe/comp2025/symboltable/JmmSymbolTableBuilder.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import static pt.up.fe.comp2025.ast.Kind.*;
 
@@ -40,11 +42,12 @@ public class JmmSymbolTableBuilder {
 
         var imports = buildImports(root);
 
-        // TODO: After your grammar supports more things inside the program (e.g., imports) you will have to change this
-        var classDecl = root.getChild(imports.size()); // TODO: melhorar isto
+        var classDecl = root.getChildren(CLASS_DECL).getFirst();
         SpecsCheck.checkArgument(Kind.CLASS_DECL.check(classDecl), () -> "Expected a class declaration: " + classDecl);
+
         String className = classDecl.get("name");
         String superClassName = classDecl.getOptional("superClass").orElse(null);
+
         var methods = buildMethods(classDecl);
         var returnTypes = buildReturnTypes(classDecl);
         var params = buildParams(classDecl);
@@ -78,11 +81,27 @@ public class JmmSymbolTableBuilder {
 
         for (var method : classDecl.getChildren(METHOD_DECL)) {
             var name = method.get("name");
-            var params = method.getChildren(PARAM).stream()
-                    // TODO: When you support new types, this code has to be updated
-                    //.map(param -> new Symbol(TypeUtils.newIntType(), param.get("name")))
-                    .map(param -> new Symbol(TypeUtils.convertType(param.getChild(0)), param.get("name")))
-                    .toList();
+            Set<String> paramNames = new HashSet<>();
+            List<Symbol> params = new ArrayList<>();
+
+            for (var param : method.getChildren(PARAM)) {
+                var paramName = param.get("name");
+
+                if (!paramNames.add(paramName)) {
+                    // Create error report for duplicate parameter
+                    var message = String.format("Duplicate parameter '%s' in method '%s'.", paramName, name);
+                    reports.add(Report.newError(
+                            Stage.SEMANTIC,
+                            param.getLine(),
+                            param.getColumn(),
+                            message,
+                            null)
+                    );
+                }
+
+                var paramType = TypeUtils.convertType(param.getChild(0));
+                params.add(new Symbol(paramType, paramName));
+            }
 
             map.put(name, params);
         }
@@ -91,16 +110,31 @@ public class JmmSymbolTableBuilder {
     }
 
     private Map<String, List<Symbol>> buildLocals(JmmNode classDecl) {
-
         var map = new HashMap<String, List<Symbol>>();
 
         for (var method : classDecl.getChildren(METHOD_DECL)) {
             var name = method.get("name");
-            var locals = method.getChildren(VAR_DECL).stream()
-                    // TODO: When you support new types, this code has to be updated
-                    .map(varDecl -> new Symbol(TypeUtils.convertType(varDecl.getChild(0)), varDecl.get("name")))
-                    .toList();
+            Set<String> localNames = new HashSet<>();
+            List<Symbol> locals = new ArrayList<>();
 
+            for (var local : method.getChildren(VAR_DECL)) {
+                var localName = local.get("name");
+
+                if (!localNames.add(localName)) {
+                    // Create error report for duplicate local variable
+                    var message = String.format("Duplicate local variable '%s' in method '%s'.", localName, name);
+                    reports.add(Report.newError(
+                            Stage.SEMANTIC,
+                            local.getLine(),
+                            local.getColumn(),
+                            message,
+                            null)
+                    );
+                }
+
+                var localType = TypeUtils.convertType(local.getChild(0));
+                locals.add(new Symbol(localType, localName));
+            }
 
             map.put(name, locals);
         }
@@ -109,19 +143,48 @@ public class JmmSymbolTableBuilder {
     }
 
     private List<String> buildMethods(JmmNode classDecl) {
+        List<String> methods = new ArrayList<>();
+        Set<String> methodNames = new HashSet<>();
 
-        var methods = classDecl.getChildren(METHOD_DECL).stream()
-                .map(method -> method.get("name"))
-                .toList();
+        for (var method : classDecl.getChildren(METHOD_DECL)) {
+            var methodName = method.get("name");
+
+            if (!methodNames.add(methodName)) {
+                // Create error report for duplicate method
+                var message = String.format("Duplicate method '%s'.", methodName);
+                reports.add(Report.newError(
+                        Stage.SEMANTIC,
+                        method.getLine(),
+                        method.getColumn(),
+                        message,
+                        null)
+                );
+            }
+
+            methods.add(methodName);
+        }
 
         return methods;
     }
 
     private List<Symbol> buildFields(JmmNode classDecl) {
         List<Symbol> fields = new ArrayList<>();
-
+        Set<String> fieldNames = new HashSet<>();
         for (var field : classDecl.getChildren(VAR_DECL)) {
             var name = field.get("name");
+
+            if (!fieldNames.add(name)) {
+                // Create error report
+                var message = String.format("Duplicate field '%s'.", name);
+                reports.add(Report.newError(
+                        Stage.SEMANTIC,
+                        field.getLine(),
+                        field.getColumn(),
+                        message,
+                        null)
+                );
+            }
+
             var typeNode = field.getChild(0);
             var fieldType = TypeUtils.convertType(typeNode);
             fields.add(new Symbol(fieldType, name));
@@ -132,10 +195,26 @@ public class JmmSymbolTableBuilder {
 
     private List<String> buildImports(JmmNode root) {
         List<String> imports = new ArrayList<>();
+        Set<String> importClassNames = new HashSet<>();
 
         for (var importDecl : root.getChildren(IMPORT_DECL)) {
             var parts = importDecl.get("path");
-            imports.add(String.join(".", parts.substring(1, parts.length() - 1).split(",")));
+            String importPath = String.join(".", parts.substring(1, parts.length() - 1).split(",")).trim();
+            String className = importPath.substring(importPath.lastIndexOf('.') + 1);
+
+            if (!importClassNames.add(className)) {
+                // Create error report for duplicate import class
+                var message = String.format("Duplicate import class '%s'.", className);
+                reports.add(Report.newError(
+                        Stage.SEMANTIC,
+                        importDecl.getLine(),
+                        importDecl.getColumn(),
+                        message,
+                        null)
+                );
+            } else {
+                imports.add(importPath);
+            }
         }
 
         return imports;
